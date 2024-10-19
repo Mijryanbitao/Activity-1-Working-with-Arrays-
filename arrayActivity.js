3d318200d75776b9548599f8284e12bdb37bd061
Mijryanbitao <mijryanb@gmail.com>
2024-10-19 18:12:54 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+// Favorite Singers
+let favSingers = ["Dionela Jay R", "Rob Daniel", "River Maya"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["04", "09", "20", "30"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
